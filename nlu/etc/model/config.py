ab6394cf2a1e6de32829a55a947248ceb1321778
Shakhyar Gogoi <68093713+shakhyar@users.noreply.github.com>
2021-07-28 17:14:29 +0530

    Add files via upload

---
+++ nlu/etc/model/config.py
@@ -0,0 +1,4 @@
+vocab_size = 1000
+embedding_dim = 16
+max_len = 20
+oov_token = "<OOV>"
---
+++ nlu/etc/model/train.py
@@ -0,0 +1,5 @@
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+sequences = tokenizer.texts_to_sequences(training_sentences)
+padded_sequences = pad_sequences(sequences, truncating='post', maxlen=max_len)
