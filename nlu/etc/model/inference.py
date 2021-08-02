1eda62e481014785c38de124078281863dc1c465
Shakhyar Gogoi <68093713+shakhyar@users.noreply.github.com>
2021-08-02 10:13:09 +0530

    Update inference.py

--- nlu/etc/model/inference.py
+++ nlu/etc/model/inference.py
@@ -1,1 +1,45 @@
-a
+import json
+import pickle
+
+import numpy as np 
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, Flatten
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from config import *
+
+
+model = load_model("saved/model.h5")
+
+with open('saved/tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+with open('saved/lbl_encoder.pickle', 'rb') as handle:
+    lbl_encoder = pickle.load(handle)
+
+
+def chat():
+	print("Start messaging with the bot (type quit to stop)!\n")
+	while True:
+		print("User: ")
+		inp = input()
+		if inp.lower() == "quit":
+			break
+
+		result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),
+                                             truncating='post', maxlen=max_len))
+		tag = lbl_encoder.inverse_transform([np.argmax(result)])
+
+		for i in data['intents']:
+			if i['tag'] == tag:
+				print("ChatBot:" + np.random.choice(i['responses']))
+				print("intent asked for: ", i['context_set'], "\n")
+
+        # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL,random.choice(responses))
+
+chat()
