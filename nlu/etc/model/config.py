6d6e42a63e0ad4f28d243308c18f62ca1bf7e05c
Shakhyar Gogoi <68093713+shakhyar@users.noreply.github.com>
2021-08-04 08:11:16 +0530

    Update config.py

--- nlu/etc/model/config.py
+++ nlu/etc/model/config.py
@@ -3,5 +3,6 @@ epochs = 1000
 embedding_dim = 16
 max_len = 20
 oov_token = "<OOV>"
+data_path = "/content/NLU_chat/nlu/etc/model/data/data.json"
 max_headlines = 5
 weather_api_key = 'f317f1f507f2d9f0a8aa1316d86507b8'
